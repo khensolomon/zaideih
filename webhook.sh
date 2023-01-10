50f2879b9b32aef2848caa8119be28d168e86410
Khen Solomon Lethil <khensolomon@gmail.com>
2023-01-10 15:34:01 +0100

    git pull -f origin master

--- webhook.sh
+++ webhook.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-git update-index --chmod=+x webhook.sh
+# Copy to
+# cp webhook.sh /var/www/webhook/update-zaideih.sh
+
+# Make it executable
+# chmod +x /var/www/webhook/update-zaideih.sh
+# And then modify /etc/webhook.conf
+
+# git update-index --chmod=+x webhook.sh
+
+# Update repo
 git pull -f origin master
 
+# Install dependencies
 npm install
+# Build assets
 npm run build
 
+# Reload PM2
 pm2 reload Zaideih
+# Save PM2
 pm2 save
