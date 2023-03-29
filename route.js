96e0fb42fb01998bd4580accebb382bce682e741
Khen Solomon Lethil <khensolomon@gmail.com>
2023-03-29 07:27:17 +0200

    sort

--- route.js
+++ route.js
@@ -1,26 +1,2 @@
-// import core from 'lethil';
-import "./routes/home.js";
-// import './routes/about.js';
-// import './routes/grammar.js';
-// import './routes/fonts.js';
-// import './routes/dictionary.js';
-// import './routes/definition.js';
-
-// import './routes/privacy.js';
-// import './routes/terms.js';
 import "./routes/api.js";
-
-/*
-app.Navigation('navAPI').route({url: '/api',route: 'api', text: 'API'});
-
-// app.Navigation('navTerms')
-//   .route({url: '/privacy',route: 'home', text: 'Privacy'})
-//   .route({url: '/terms',route: 'home', text: 'Terms'});
-
-app.Navigation('navPage')
-  .route({url: '/',route: 'home', text: 'Home'});
-  // .route({url: '/about',route: 'about', text: 'About'})
-  // .route({url: '/music',route: 'music', text: 'Music'});
-
-app.Navigation('navFallback').route({url: '*',route: 'home', text: 'Fallback'});
-*/
+import "./routes/home.js";
