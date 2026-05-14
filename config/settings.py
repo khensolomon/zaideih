3ade5ae00e8e810e9bc45865abbfbf95123d13d2
khensolomon <1753766+khensolomon@users.noreply.github.com>
2026-05-14 08:49:29 +0200

    deploy: [ssh] v5 update database user retrieval and add connection max age

--- config/settings.py
+++ config/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
+        'USER': os.getenv('DB_USER', 'admin'),
         'PASSWORD': os.environ.get('DB_PWD'),
         'HOST': os.environ.get('DB_HOST', 'db'),
         'PORT': os.environ.get('DB_PORT', '3306'),
@@ -101,6 +101,7 @@ DATABASES = {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
         },
+        'CONN_MAX_AGE': 60,
     }
 }
 
