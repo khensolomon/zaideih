e1f34e9e4e7c35b4ca853d1e44ecc88f14f7c67a
khensolomon <1753766+khensolomon@users.noreply.github.com>
2026-06-19 11:41:36 +0200

    Update queries

---
+++ assets/db-query/help-clean.sql
@@ -0,0 +1,6 @@
+-- <app>
+-- SELECT ROW_COUNT() AS track;
+-- SELECT ROW_COUNT() AS file;
+
+SELECT COUNT(*) FROM track;
+SELECT COUNT(*) FROM file;
--- assets/db-query/help.sql
+++ assets/db-query/help.sql
@@ -1,1 +1,3 @@
 -- <app>
+SELECT ROW_COUNT() AS track;
+SELECT ROW_COUNT() AS file;
